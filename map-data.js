6ce2de608e0cfc9133812eec9507daddee76e0ed
Hexvenn <hexvenn@gmail.com>
2025-05-05 20:38:54 +0200

    Initial version

---
+++ map-data.js
@@ -0,0 +1,13 @@
+const maps = [
+    {
+        name: "Darkshore",
+        image: "maps/darkshore.jpg",
+        points: [
+            { text: "lvl 11-20", x: 37, y: 49 },
+            { text: "lvl 11-20", x: 44, y: 59 },
+            { text: "lvl 11-20", x: 43, y: 67 },
+            { text: "lvl 11-20", x: 38, y: 83 },
+            { text: "lvl 11-20", x: 37, y: 93 },
+        ],
+    }
+];
